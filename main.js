4360044edde1ff6c7dc2cc7c383bd76e91c9edd6
jfasebook <jfasebook@users.noreply.github.com>
2021-09-12 20:02:03 +0200

    fix another

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 addEventListener("fetch", (event) => {
-  let response = new Response('', {status: 301})
+  const response = new Response("<html> Hello </html>", {
+  status: 200,
+  headers: {
+    "content-type": "text/html",
+  },
+});
+  console.log(JSON.stringify(response);
+  redirect(url: string, status?: number)
   event.respondWith(
-    response.redirect('https://www.google.es#thisATest'),
+    response,
   );
 });
