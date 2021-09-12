a845700fbedf8c4ed4e9ceb9f1f4afbcf0e504f2
jfasebook <jfasebook@users.noreply.github.com>
2021-09-12 20:37:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-// addEventListener("fetch", (event) => {
-//   const response = new Response("<html> Hello </html>", {
-//     status: 200,
-//     headers: {
-//       "content-type": "text/html",
-//     },
-//   };
-                                
-//   console.log(JSON.stringify(response));
-//   event.respondWith(
-//     response,
-//   );
-// });
-
-
-// Create a response with html as its body.
-const response = new Response("<html> Hello </html>", {
-  status: 200,
-  headers: {
-    "content-type": "text/html",
-  },
-});
-
-console.log(response.status); // 200
-console.log(response.headers.get("content-type")); // text/html
-
 // You can pass the instance to respondWith() to respond to request
 // with the above response.
 addEventListener("fetch", (event) => {
