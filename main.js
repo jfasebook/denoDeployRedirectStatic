20fc430e04ed60870fe26ca6f0cf1504c4fd75aa
jfasebook <jfasebook@users.noreply.github.com>
2021-09-12 20:11:22 +0200

    fix

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 addEventListener("fetch", (event) => {
   const response = new Response("<html> Hello </html>", {
-  status: 200,
-  headers: {
-    "content-type": "text/html",
-  },
-});
+    status: 200,
+    headers: {
+      "content-type": "text/html",
+    },
+  };
+                                
   console.log(JSON.stringify(response));
   event.respondWith(
     response,
   );
 });
+  
+
